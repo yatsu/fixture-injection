6f8e18d0c30214e40367aaef1fe7969c2479022d
Masaki Yatsu <yatsu@yatsu.info>
2018-11-24 20:44:14 +0900

    Add tests for jasmine-fixture-injection

---
+++ packages/jasmine-fixture-injection-tests/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  plugins: ['jasmine'],
+  env: {
+    jasmine: true
+  },
+  extends: ['airbnb-base', 'plugin:jasmine/recommended'],
+  globals: {
+    fixture: true,
+    useFixture: true,
+    nonuse: true
+  },
+  rules: {
+    'class-methods-use-this': 'off',
+    'comma-dangle': ['error', 'never'],
+    'no-console': 'off',
+    'no-mixed-operators': 'off',
+    'no-param-reassign': ['error', { props: false }],
+    'no-shadow': 'off',
+    semi: ['error', 'never'],
+    'import/no-extraneous-dependencies': [
+      'error',
+      { devDependencies: ['**/*.spec.js', '**/spec/*[Ff]ixtures.js', '**/scripts/test-*'] }
+    ],
+    'import/prefer-default-export': 'off',
+    'jasmine/no-spec-dupes': 'off',
+    'jasmine/no-suite-callback-args': 'off' // required by fixture injection
+  }
+}
---
+++ packages/jasmine-fixture-injection-tests/async/async.spec.js
@@ -0,0 +1,65 @@
+const { sleep } = require('./helper')
+
+describe('Single fixture in a test case', () => {
+  it('a', (a) => {
+    expect(a).toEqual('A()')
+  })
+
+  it('d', (d) => {
+    expect(d).toEqual('D(A(),B())')
+  })
+
+  it('g', async (g) => {
+    await sleep(1)
+
+    expect(g).toEqual('G(C(),D(A(),B()))')
+  })
+})
+
+describe('Multiple fixtures in a test case', () => {
+  it('i, k and l', (i, k, l) => {
+    expect(i).toEqual('I(D(A(),B()),E())')
+    expect(k).toEqual('K(F(C()),H())')
+    expect(l).toEqual('L(H(),J(E()))')
+  })
+})
+
+describe('Single fixture in a test suite', () => {
+  useFixture((a) => {
+    this.a = a
+  })
+
+  useFixture((d) => {
+    this.d = d
+  })
+
+  useFixture(async (g) => {
+    await sleep(1)
+
+    this.g = g
+  })
+
+  it('a, d and g', () => {
+    const { a, d, g } = this
+
+    expect(a).toEqual('A()')
+    expect(d).toEqual('D(A(),B())')
+    expect(g).toEqual('G(C(),D(A(),B()))')
+  })
+})
+
+describe('Multiple fixtures in a test suite', () => {
+  useFixture((i, k, l) => {
+    this.i = i
+    this.k = k
+    this.l = l
+  })
+
+  it('i, k and l', () => {
+    const { i, k, l } = this
+
+    expect(i).toEqual('I(D(A(),B()),E())')
+    expect(k).toEqual('K(F(C()),H())')
+    expect(l).toEqual('L(H(),J(E()))')
+  })
+})
